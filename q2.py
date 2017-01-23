5f2c63e8508e5f7ffac80990559e9773044c26a8
Himanshu Mishra <himanshu2014iit@gmail.com>
2017-01-23 13:22:03 +0530

    Sum of sqrt upto 2 digits

---
+++ q2.py
@@ -0,0 +1,22 @@
+try:
+    input = raw_input
+except:
+    pass
+
+def return_sum_of_sqrt(no):
+    num = "{0:.2f}".format(round(no**0.5, 2))
+    sum = 0
+    for digit in num:
+        try:
+            sum += int(digit)
+        except:
+            pass
+
+    return sum
+
+if __name__ == '__main__':
+    num = input()
+    try:
+        print(return_sum_of_sqrt(int(num)))
+    except:
+        print(0)
